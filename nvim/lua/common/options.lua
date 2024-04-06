e064ad2ff9759c6a1cd2d720b0b43cdde850f313
LUH <unghee66@gmail.com>
2024-04-06 23:23:49 +0900

    feature; add additional options

--- nvim/lua/common/options.lua
+++ nvim/lua/common/options.lua
@@ -13,11 +13,16 @@ opt.showmode = false
 opt.clipboard = "unnamedplus"
 opt.cursorline = true
 
-opt.expandtab = true
 opt.shiftwidth = 2
 opt.smartindent = true
+opt.breakindent = true
+
+opt.expandtab = true
 opt.tabstop = 2
 opt.softtabstop = 2
+
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", eol = "↵" }
 
 opt.fillchars = { eob = " " }
 opt.ignorecase = true
@@ -29,4 +34,7 @@ opt.relativenumber = true
 opt.numberwidth = 2
 opt.ruler = false
 
+opt.undofile = true
+
+opt.updatetime = 250
 
