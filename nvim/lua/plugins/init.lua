26d58b18ada2c78a86a06aa9b4aad6b5ce8586a2
unghee.lee <unghee.lee@navercorp.com>
2025-02-10 13:06:01 +0900

    fix; fix typo

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-lua/plenary.nvim",
   {
-	  "folke/neodev.nvim",
+    "folke/neodev.nvim",
     opts = {}
   },
   {
@@ -9,37 +9,37 @@ return {
     config = function ()
     end
   },
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy"
-	},
-	{
-		'maxmx03/dracula.nvim',
-		lazy = false,
-		priority = 1000,
-		config = function ()
-			local dracula = require 'dracula'
-			dracula.setup()
-			vim.cmd.colorscheme 'dracula'
-		end
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		-- cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require "nvim-treesitter.configs"
-			configs.setup({
-				ensure_installed = { "lua", "vim", "vimdoc" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy"
+  },
+  {
+    'maxmx03/dracula.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function ()
+      local dracula = require 'dracula'
+      dracula.setup()
+      vim.cmd.colorscheme 'dracula'
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require "nvim-treesitter.configs"
+      configs.setup({
+        ensure_installed = { "lua", "vim", "vimdoc" },
         auto_install = true,
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
-				indent = { enable = true }
-			})
-		end
-	},
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+        },
+        indent = { enable = true }
+      })
+    end
+  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
